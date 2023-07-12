91a0a11c1858ec4d498793e78298ce3a0520d828
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-07-11 19:45:22 -0500

    Rendering results in correct section

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 movieTitle = "";
 
 
-document.getElementById("search-button").addEventListener("click", searchForMovie)
 
+document.getElementById("search-button").addEventListener("click", searchForMovie)
+mainSectionDisplay = document.getElementById("main-section");
 
 
 function searchForMovie(){
     movieTitle = document.getElementById("search").value;
-    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=36b0b502&s=${movieTitle}`)
-    .then(response => response.json())
-    .then(data => console.log(data))
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=36b0b502&s=${movieTitle}&page=1`)
+    .then( response => response.json())
+    .then(data => {
+        clearMovieArea();
+        getMovieInformation(data.Search)
+    })
+}
+
+function getMovieInformation(movieArray){
+    movieArray.forEach(function(movie){
+        fetch(`http://www.omdbapi.com/?apikey=36b0b502&i=${movie.imdbID}`)
+        .then(response => response.json())
+        .then(data => renderMovie(data))
+    })
+}
+
+function clearMovieArea(){
+    mainSectionDisplay.innerHTML = "";
 }
 
 
+function renderMovie(movie){
+        mainSectionDisplay.innerHTML +=
+            `<div class="movie">
+                <img class="movie-poster" src=${movie.Poster} alt="">
+                <div class="details">
+                    <div class="title">
+                        <h2 class="movie-title">${movie.Title}</h2>
+                        <img src="./assets/Star.png" alt="">
+                        <h2 class="rating">${movie.imdbRating}</h2>
+                    </div>
+                    <div class="extra-details">
+                        <h3 class="runtime">${movie.Runtime}</h3>
+                        <h3 class="genre">${movie.Genre}</h3>
+                        <div class="add-to-watchlist-section">
+                            <button class="watchlist-btn"><img src="./assets/plus.png" alt="" class="add-img"></button>
+                            <h3 class="watchlist-text">Watchlist</h3>
+                        </div>
+                    </div>
+                    <p class="movie-description">${movie.Plot}</p>
+                    
+                    
+                </div>
 
-function renderList(movieArray){
-
+            </div>`
 
 }
